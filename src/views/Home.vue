967cdc7d1c37470d284502ba36b0cdfed76074ea
jazzyclimber <jazzyclimber@gmail.com>
2022-02-19 12:33:38 -0600

    Modify layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="home">
-    <div class="sidebar">
-      <MenuTree v-if="workingTree[0] != null" :localTree="workingTree" :level="1"/>
-    </div>
-    <main>
-      <UploadFile />
-      <div class="field-container">
-        <NewDisplay  />
-        <FieldEditor  />
-      </div>
-    </main>
+  <multipane class="custom-resizer" layout="vertical">
+  <div class="pane sidebar" :style="{ flexGrow:0}">
+    <UploadFile />
+    <MenuTree v-if="workingTree[0] != null" :localTree="workingTree" :level="1"/>
+  </div>
+  <multipane-resizer></multipane-resizer>
+  <div class="pane field-display-wrapper" :style="{ flexGrow: 0 }">
+    <NewDisplay  />
+  </div>
+  <multipane-resizer></multipane-resizer>
+  <div class="pane field-editor-wrapper" :style="{ flexGrow: 1 }">
+    <FieldEditor  />
+  </div>
+</multipane>
+</Multipane>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { mapGetters } from 'vuex'
+import { Multipane, MultipaneResizer } from 'vue-multipane';
 import UploadFile from '@/components/UploadFile.vue'
 import MenuTree from "@/components/MenuTree"
 // import DisplayFile from "@/components/displayFile/DisplayFile"
@@ -27,7 +33,9 @@ export default {
     UploadFile,
     MenuTree,
     NewDisplay,
-    FieldEditor
+    FieldEditor,
+    Multipane,
+    MultipaneResizer
   },
    computed: {
      ...mapGetters(["tree"]),
@@ -38,17 +46,17 @@ export default {
 }
 </script>
 
-<style>
+<style >
   *,*:before,*:after {
     box-sizing: border-box;
   }
+
   .sidebar {
     background-color: #f2f2f2;
     padding-right: 10px;
-    min-width: 200px;
-    min-height: 100%;
-    max-height: 100%;
-    overflow: auto;
+  }
+  .field-editor-wrapper {
+    background-color: #f2f2f2;
   }
   main {
     max-height: 100vh;
@@ -78,4 +86,44 @@ export default {
     overflow: auto;
     min-height: 100%;
   }
+  .custom-resizer > .multipane-resizer {
+  margin: 0; left: 0;
+  position: relative;
+  }
+.custom-resizer > .multipane-resizer:before {
+  display: block;
+  content: "";
+  width: 3px;
+  height: 40px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -20px;
+  margin-left: -1.5px;
+  border-left: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+}
+.custom-resizer > .multipane-resizer:hover:before {
+  border-color: #999;
+}
+.custom-resizer {
+  width: 100%;
+  height: 100%;
+}
+.custom-resizer > .pane {
+  text-align: left;
+  padding: 15px;
+  overflow: auto;
+  border: 1px solid #ccc;
+  height: 100vh;
+}
+.pane::-webkit-scrollbar {
+  background: transparent;        /* color of the tracking area */
+  width: 5px;
+  height: 5px;
+}
+.pane::-webkit-scrollbar-thumb {
+  background-color: rgba(0,0,0,.3);    /* color of the scroll thumb */
+  border-radius: 20px;       /* roundness of the scroll thumb */
+}
 </style>
