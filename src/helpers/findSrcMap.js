6202bf80ecf483980f0c15aa738f05ecefaf0bdc
jazzyclimber <jazzyclimber@gmail.com>
2022-08-25 13:28:30 -0500

    move into own file

---
+++ src/helpers/findSrcMap.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+function FindSrcMap(openDir) {
+  const srcmapPath = path.join(openDir , "COPILOT.srcmap.json");
+  // console.log(srcmapPath);
+  if (fs.existsSync(srcmapPath)) {
+    console.log('Source Map Exists');
+    return {srcMapPath: srcmapPath, srcMapData: JSON.parse(fs.readFileSync(srcmapPath))}
+  } else {
+    console.log('Source Map Not Found');
+    return {srcMapPath: srcmapPath, srcMapData: undefined};
+  }
+}
+
+module.exports = {
+  FindSrcMap
+}
